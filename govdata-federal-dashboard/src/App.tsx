b887ab16b398fbc29e301d9c2d02829f62c73c4d
Carl Gödecken <carl.goedecken@icloud.com>
2023-11-15 11:52:18 +0100

    Working basic visualization

--- govdata-federal-dashboard/src/App.tsx
+++ govdata-federal-dashboard/src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import { ResponsiveTreeMapHtml } from "@nivo/treemap";
+
+interface DataContributionMeta {
+  department: string;
+  description: string;
+  datasets: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<DataContributionMeta[] | null>(null);
+
+  useEffect(() => {
+    fetch("/backend-response.json")
+      .then((data) => data.json())
+      .then((data) => setData(data as DataContributionMeta[]));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        Data sets provided to <a href="https://www.govdata.de/">GovData.de</a>{" "}
+        by federal ministries
+      </h1>
+      {data ? <Treemap data={data} /> : <div>Loading...</div>}
     </>
-  )
+  );
 }
 
-export default App
+const Treemap: React.FC<{ data: DataContributionMeta[] }> = ({ data }) => {
+  const isMinistry = (datum: DataContributionMeta) =>
+    datum.department.includes("Bundesministerium");
+
+  const rootDataElement = {
+    children: [
+      { department: "Federal ministries", children: data.filter(isMinistry) },
+      { department: "Other", children: data.filter((d) => !isMinistry(d)) },
+    ],
+  };
+  return (
+    <div className="treemap-container">
+      {" "}
+      <ResponsiveTreeMapHtml
+        data={rootDataElement}
+        identity="department"
+        value="datasets"
+        label={"id"}
+        margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
+        labelSkipSize={12}
+        labelTextColor={{
+          from: "color",
+          modifiers: [["darker", 2]],
+        }}
+        outerPadding={8}
+        parentLabelTextColor={{
+          from: "color",
+          modifiers: [["darker", 3]],
+        }}
+        colors={{ scheme: "yellow_orange_red" }}
+        borderColor={{
+          from: "color",
+          modifiers: [["darker", 0.1]],
+        }}
+      />
+    </div>
+  );
+};
+
+export default App;
